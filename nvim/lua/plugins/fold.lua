117e14aa3af050423ea2e299548ef7fe1511c36c
anhoder <anhoderai@xiaoman.cn>
2024-03-14 22:28:55 +0800

    fix ufo error

--- nvim/lua/plugins/fold.lua
+++ nvim/lua/plugins/fold.lua
@@ -28,7 +28,9 @@ return {
 
     require("ufo").setup({
       open_fold_hl_timeout = 150,
-      close_fold_kinds_for_ft = { "imports" },
+      close_fold_kinds_for_ft = {
+        default = { "imports" },
+      },
       provider_selector = function(bufnr, filetype, buftype)
         return ftMap[filetype] or customizeSelector
       end,
