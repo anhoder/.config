5c4dc08a1769d964db0a0ba3f2af9b826c1940e4
anhoder <anhoder@88.com>
2024-04-08 19:19:15 +0800

    feat(neovim): add goneovim theme diff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,10 @@ vim.g.gruvbox_italicize_strings = 1
 vim.g.gruvbox_invert_selection = 0
 vim.g.gruvbox_invert_signs = 0
 vim.g.gruvbox_improved_warnings = 1
+if vim.g.goneovim then
+  vim.g.gruvbox_contrast_dark = "hard"
+  vim.g.gruvbox_contrast_light = "hard"
+end
 
 return {
   -- add gruvbox
