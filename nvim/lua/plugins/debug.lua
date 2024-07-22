0cbe35346ee0c2689231338f864842bed54ef54f
anhoder <anhoder@88.com>
2024-07-22 22:59:50 +0800

    chore: upgrade deps

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -187,18 +187,18 @@ return {
       { "<leader>tL", function() require('dap-go').debug_last_test() end, desc = "Debug Last (Go)" },
     },
 
-    config = function()
-      local Config = require("lazyvim.config")
-      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-      for name, sign in pairs(Config.icons.dap) do
-        sign = type(sign) == "table" and sign or { sign }
-        vim.fn.sign_define(
-          "Dap" .. name,
-          --- @diagnostic disable-next-line
-          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-        )
-      end
-    end,
+    -- config = function()
+    --   local Config = require("lazyvim.config")
+    --   vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+    --
+    --   for name, sign in pairs(Config.icons.dap) do
+    --     sign = type(sign) == "table" and sign or { sign }
+    --     vim.fn.sign_define(
+    --       "Dap" .. name,
+    --       --- @diagnostic disable-next-line
+    --       { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+    --     )
+    --   end
+    -- end,
   },
 }
