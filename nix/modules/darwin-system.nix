a3af02e89dec3d7d959f46f0fccf3860b7b9880c
anhoder <anhoder@88.com>
2024-11-05 08:55:02 +0800

    feat(nix): add darwin setting

--- nix/modules/darwin-system.nix
+++ nix/modules/darwin-system.nix
@@ -19,19 +19,53 @@
     '';
 
     defaults = {
+      NSGlobalDomain = {
+        AppleShowAllFiles = true;
+        AppleEnableMouseSwipeNavigateWithScrolls = false;
+        AppleEnableSwipeNavigateWithScrolls = false;
+        AppleFontSmoothing = 1;
+        AppleICUForce24HourTime = true;
+        AppleShowAllExtensions = true;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSScrollAnimationEnabled = true;
+        InitialKeyRepeat = 10;
+        KeyRepeat = 1;
+        "com.apple.swipescrolldirection" = true;
+      };
+      dock = {
+        persistent-others = [ "~/Downloads" ];
+      };
+      finder = {
+        AppleShowAllFiles = true;
+        ShowStatusBar = true;
+        ShowPathbar = true;
+        AppleShowAllExtensions = true;
+        _FXShowPosixPathInTitle = true;
+        _FXSortFoldersFirst = true;
+      };
+      ActivityMonitor = {
+        ShowCategory = 103;
+        IconType = 3;
+        SortColumn = "CPUUsage";
+        SortDirection = 0;
+        OpenMainWindow = true;
+      };
       menuExtraClock.Show24Hour = true; # show 24 hour clock
-
-      # other macOS's defaults configuration.
-      # ......
     };
   };
 
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # this is required if you want to use darwin's default shell - zsh
-  programs.zsh.enable = false;
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
+  environment.shells = [ pkgs.fish ];
 
   services.nix-daemon.enable = true;
 }
